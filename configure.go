90dbd57624e842b7a7b99cbff1d60dfc8e5fb1b1
孙文吉 <sunwenji@xiaoniangao.com>
2021-12-14 18:27:37 +0800

    feat:完善匹配模式

--- configure.go
+++ configure.go
@@ -1,11 +1,23 @@
 package configure
 
+import "strings"
+
 // Path 路径信息
 type Path struct {
 	AppID     string
 	NameSpace string
 	Key       string
 	Index     uint64 // CAS
+	// 0--精准匹配 1--模糊匹配
+	MatchRule int8
+}
+
+// Match 检查是否匹配
+func (p Path) Match(k string) bool {
+	if p.MatchRule == 0 {
+		return p.Key == k
+	}
+	return strings.Contains(k, p.Key)
 }
 
 // Client 这里抽象配置中心
